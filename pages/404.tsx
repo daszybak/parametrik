dce09fd7c0a2c95307961490addc6aeb50e791ff
daszybak <karlo1diidd@gmail.com>
2022-06-20 15:30:46 +0200

    fixed link import

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,12 @@
-import Link from 'next/Link'
+import Link from "next/link";
 
 export default function FourOhFour() {
-    return <div>
-        <h1>Page Not Found</h1>
-        <Link href="/">
-            <a>Go Home</a>
-        </Link>
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <Link href="/">
+        <a>Go Home</a>
+      </Link>
     </div>
+  );
 }
