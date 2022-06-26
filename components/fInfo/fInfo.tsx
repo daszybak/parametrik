83f51e007ef9618e2af24a0930f7bcc6f376906f
daszybak <karlo1diidd@gmail.com>
2022-06-27 01:02:26 +0200

    last commit  on laptop

---
+++ components/fInfo/fInfo.tsx
@@ -0,0 +1,14 @@
+import styles from "./finfo.module.scss";
+
+const FInfo = () => {
+  return (
+    <div className={styles.fInfo}>
+      <div className={styles.col}>
+        <span>CONTACT</span>
+        <div className={styles.row}></div>
+      </div>
+    </div>
+  );
+};
+
+export default FInfo;
