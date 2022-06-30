c163ddf496db79cc1130e71f52e5a0042c79ce6f
daszybak <karlo1diidd@gmail.com>
2022-06-30 23:11:56 +0200

    fixed video size

--- components/loadingVideo/loadingVideo.tsx
+++ components/loadingVideo/loadingVideo.tsx
@@ -1,10 +1,13 @@
 import { useContext, useEffect, useRef } from "react";
 import VideoLoadContext from "../../context/videoLoad/videoLoad";
+import useWindowSize from "../../utils/hooks/useWindowSize";
 
 import styles from "./loadingVideo.module.scss";
 
 const LoadingVideo = () => {
   const videoContainer = useRef<HTMLVideoElement>(null);
+  const width = useWindowSize().width;
+
   const {
     handleVideoStarted,
     handleVideoLoaded: handleVideoLoadedCxt,
@@ -36,10 +39,15 @@ const LoadingVideo = () => {
         video.removeEventListener("ended", handleVideoEnded);
       }
     };
-  });
+  }, [handleVideoStarted, handleVideoLoadedCxt, handleVideoFinished]);
 
   return (
-    <video height="auto" width="100%" muted ref={videoContainer}>
+    <video
+      height="auto"
+      width={typeof width !== "undefined" && width < 1280 ? "100%" : 1280}
+      muted
+      ref={videoContainer}
+    >
       <source src="/videos/loading3sec.mp4" type="video/mp4" />
     </video>
   );
